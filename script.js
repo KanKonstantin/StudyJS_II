fc1bd38d287054c93649b68674bcc6479399cc53
KanKonstantin <constantine.kan@bk.ru>
2022-05-24 20:08:59 +0500

    2nd HomeWork

--- script.js
+++ script.js
@@ -4,17 +4,74 @@ const goods = [
   { title: 'Jacket', price: 350 },
   { title: 'Shoes', price: 250 },
 ];
+class cart {
 
-const renderGoodsItem =  ({title = '', price = 0}) => `
-    <div class="goods-item">
-      <h3>${title}</h3>
-      <p>${price}</p>
-    </div>
-`;
+}
+class cartElement {
 
-const renderGoodsList = (list = []) => {
-  let goodsList = list.map(item => renderGoodsItem(item));
-  document.querySelector('.goods-list').innerHTML = goodsList.join('');
 }
 
-renderGoodsList(goods);
+class GoodsItem {
+  constructor({ title, price }) {
+    this.title = title;
+    this.price = price;
+  }
+  render() {
+    return `
+    <div class="goods-item">
+      <h3>${this.title}</h3>
+      <p>${this.price}</p>
+    </div>
+  `;
+  }
+}
+class GoodsList {
+  items = [];
+  fetchGoods() {
+    this.items = goods;
+  }
+  render() {
+    const goods = this.items.map(item => {
+      const goodItem = new GoodsItem(item);
+      return goodItem.render()}).join('');
+  
+    document.querySelector('.goods-list').innerHTML = goods;
+  }
+}
+class Hamburger {
+  constructor(size, stuffing) {  }
+  addTopping(topping) {} // Добавить добавку 
+  removeTopping(topping) {} // Убрать добавку 
+  getToppings(topping) {} // Получить список добавок 
+  getSize() {} // Узнать размер гамбургера 
+  getStuffing() {} // Узнать начинку гамбургера 
+  calculatePrice() {} // Узнать цену 
+  calculateCalories() {} // Узнать калорийность 
+  }
+
+class SmallHamburger extends Hamburger{
+  price = 50;
+};
+class BigHamburger extends Hamburger{
+  price = 100;
+};
+const smallHamburger = new SmallHamburger();
+const bigHamburger = new BigHamburger();
+const goodsList = new GoodsList();
+goodsList.fetchGoods();
+goodsList.render();
+
+
+// const renderGoodsItem =  ({title = '', price = 0}) => `
+//     <div class="goods-item">
+//       <h3>${title}</h3>
+//       <p>${price}</p>
+//     </div>
+// `;
+
+// const renderGoodsList = (list = []) => {
+//   let goodsList = list.map(item => renderGoodsItem(item));
+//   document.querySelector('.goods-list').innerHTML = goodsList.join('');
+// }
+
+// renderGoodsList(goods);
